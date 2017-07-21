b5c3c4e97c2e40bcab2ef09ac33d2f66f1b3b270
CCCCoder <15505514373@163.com>
2017-07-21 20:43:26 +0800

    leetcode027

---
+++ src/com/N1njaC/LeetCode028.java
@@ -0,0 +1,19 @@
+package com.N1njaC;
+
+/**
+ * Created by N1njaC on 2017/7/18.
+ */
+public class LeetCode028 {
+    public int strStr(String haystack, String needle) {
+
+        if (haystack == null || needle == null){
+            return -1;
+        }
+
+        
+
+    }
+    public static void main(String[] args){
+
+    }
+}
