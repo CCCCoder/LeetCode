30a19749c08c3505eb7d21749283e84589274bc9
CCCCoder <15505514373@163.com>
2017-08-03 21:13:49 +0800

    083

--- src/com/N1njaC/LeetCode083.java
+++ src/com/N1njaC/LeetCode083.java
@@ -14,9 +14,21 @@ public class LeetCode083 {
         }
     }
 
+    //将head赋给node来进行算法，head的next地址和node next的地址是一样的。所以后面node的变化也就是node的变化。
     public ListNode deleteDuplicates(ListNode head) {
 
-
+        if (head == null) return null;
+        ListNode node = head;
+        //这里，只要node的值和下一个的值相同的话，直接把node.next给替换成node.next.next，也就是说node.next给去除了。（这个节点消失了）
+        //不同的话继续下一个开始判断
+        while (node.next != null) {
+            if (node.val == node.next.val){
+                node.next = node.next.next;
+            }else {
+                node = node.next;
+            }
+        }
+        return head;
     }
 
     public static void main(String[] args) {
