ed65cdb65523e1eb9c2485ac0b09743d6d06d217
CCCCoder <15505514373@163.com>
2017-08-07 20:17:44 +0800

    088:Merge Sorted Array

---
+++ src/com/N1njaC/LeetCode100.java
@@ -0,0 +1,25 @@
+package com.N1njaC;
+
+/**
+ * Created by N1njaC on 2017/8/4.
+ */
+public class LeetCode100 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+//    public boolean isSameTree(TreeNode p, TreeNode q) {
+//
+//        if ((p.left == null && q.left != null) || (p.left != null && q.left == null)
+//                || (p.right == null && q.right != null) || (p.right != null && q.right == null)) {
+//            return false;
+//        } else if (p.left == null && q.left == null)
+//    }
+}
