3417b10dc439085f6db0c47856975d5007d59e13
N1njaC <aiai173cc@gmail.com>
2017-08-07 18:32:03 +0800

    leetcode100

---
+++ src/com/N1njaC/LeetCode100.java
@@ -0,0 +1,27 @@
+package com.N1njaC;
+
+/**
+ * Created by N1njaC on 2017/8/7.
+ */
+public class LeetCode100 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
