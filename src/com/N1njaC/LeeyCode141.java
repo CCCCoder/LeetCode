690266a9dc3aaa0f6dbc95e4087926201e0fd32f
N1njaC <aiai173cc@gmail.com>
2017-08-24 18:34:09 +0800

    leetcode141

---
+++ src/com/N1njaC/LeeyCode141.java
@@ -0,0 +1,33 @@
+package com.N1njaC;
+
+/**
+ * Created by N1njaC on 2017/8/24.
+ */
+public class LeeyCode141 {
+
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            this.val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) return false;
+
+        ListNode runner = head;
+        ListNode walker = head;
+
+        while (runner.next!=null&&runner.next.next!=null){
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) return true;
+        }
+        return false;
+    }
+}
